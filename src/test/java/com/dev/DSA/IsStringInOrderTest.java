b87f09175132a86bcdcde76b08be61513e62fc2e
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-16 18:46:05 +0300

    add tests to isorder class

--- src/test/java/com/dev/DSA/IsStringInOrderTest.java
+++ src/test/java/com/dev/DSA/IsStringInOrderTest.java
@@ -2,8 +2,16 @@ package com.dev.DSA;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IsStringInOrderTest {
-
+    @Test
+    public void testIsInOrder() {
+        assertFalse(IsStringInOrder.isInOrder("apc"));    // Expected: true
+        assertTrue(IsStringInOrder.isInOrder("abc")); // Expected: false
+        assertTrue(IsStringInOrder.isInOrder("123"));    // Expected: true
+        assertTrue(IsStringInOrder.isInOrder("789"));   // Expected: false
+        assertFalse(IsStringInOrder.isInOrder("xoz"));   // Expected: true
+    }
 }
