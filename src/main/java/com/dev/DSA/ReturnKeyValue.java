9037cf3821bf2fdbf10b6beffab355926155dd3e
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-17 21:14:17 +0300

    add return to value question

---
+++ src/main/java/com/dev/DSA/ReturnKeyValue.java
@@ -0,0 +1,26 @@
+package com.dev.DSA;
+
+import java.util.List;
+import java.util.Map;
+
+public class ReturnKeyValue {
+    //Return the Objects Keys and Values
+    //Create a method that takes a Map<String, String> and return the values as a separate array.
+    // Return the keys sorted alphabetically, and their corresponding values in the same order.
+    //
+    //Examples
+    //getValues({ "a": "1", "b": "2", "c": "3" } )
+    //➞ ["1", "2", "3"]
+    //
+    //getValues({ "a": "Apple", "b": "Microsoft", "c": "Google" })
+    //➞ ["Apple", "Microsoft", "Google"]
+    //
+    //getValues({ "key1": "true", "key2": "false", "key3": "undefined" })
+    //➞ ["true", "false", "undefined"]
+
+    public static List<String> getValues(Map<String, String> map){
+
+    }
+
+
+}
