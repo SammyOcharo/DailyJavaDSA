3eb77629a43037eb441afc0592a7e0516c693c3e
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-19 10:23:57 +0300

    add pentagonal number class

---
+++ src/test/java/com/dev/DSA/PentagonalNumberTest.java
@@ -0,0 +1,15 @@
+package com.dev.DSA;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PentagonalNumberTest {
+
+    @Test
+    void pentagonal() {
+
+        assertEquals(16, PentagonalNumber.pentagonal(3));
+        assertEquals(141, PentagonalNumber.pentagonal(8));
+    }
+}
