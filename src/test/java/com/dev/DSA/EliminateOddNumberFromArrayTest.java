b7ba027693311529d017c97775171f6efafa4525
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-21 22:54:11 +0300

    instant Jazz test

--- src/test/java/com/dev/DSA/EliminateOddNumberFromArrayTest.java
+++ src/test/java/com/dev/DSA/EliminateOddNumberFromArrayTest.java
@@ -12,6 +12,10 @@ class EliminateOddNumberFromArrayTest {
     void noOdds() {
         int[] expected = {718, 644, 380, 440};
         int[] results = {718, 991, 449, 644, 380, 440};
+
+        int[] expected1 = {718, 644, 380, 440};
+        int[] results2 = {718, 991, 449, 644, 380, 460};
         assertEquals(Arrays.toString(expected), Arrays.toString(EliminateOddNumberFromArray.noOdds(results)));
+        assertNotEquals(Arrays.toString(expected1), Arrays.toString(EliminateOddNumberFromArray.noOdds(results2)));
     }
 }
