b0db707c96116b932209df2e122d75a306751e15
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-24 22:16:14 +0300

    validate pin with regex pattern

--- src/test/java/com/dev/DSA/PinValidateTest.java
+++ src/test/java/com/dev/DSA/PinValidateTest.java
@@ -11,5 +11,6 @@ class PinValidateTest {
         assertTrue(PinValidate.validate("121317"));
         assertFalse(PinValidate.validate("1213170000"));
         assertFalse(PinValidate.validate(""));
+        assertTrue(PinValidate.validate("900876"));
     }
 }
