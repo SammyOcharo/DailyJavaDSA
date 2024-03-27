7c89b8ab9455aecc37042b3ed113c918db4a623d
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-27 23:23:17 +0300

    largest gap quiz

--- src/test/java/com/dev/DSA/LargestGapTest.java
+++ src/test/java/com/dev/DSA/LargestGapTest.java
@@ -13,4 +13,11 @@ class LargestGapTest {
         assertEquals("largest gap in the array is between 9 and 20 which is 11.", LargestGap.largestGap(arr));
         assertNotEquals("largest gap in the array is between 9 and 30 which is 21.", LargestGap.largestGap(arr1));
     }
+
+    @Test
+    void largestGapStreamsOption(){
+        int[] array = {9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5};
+
+        assertEquals(11, LargestGap.largestGapStreamsOption(array));
+    }
 }
