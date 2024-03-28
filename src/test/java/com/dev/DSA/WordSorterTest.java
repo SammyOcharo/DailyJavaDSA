971a1d4b9712404a3ac98c4fe1e9c1928df2e000
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-28 23:20:15 +0300

    words sorter

--- src/test/java/com/dev/DSA/WordSorterTest.java
+++ src/test/java/com/dev/DSA/WordSorterTest.java
@@ -11,5 +11,6 @@ class WordSorterTest {
 
         assertEquals("my Hello friend", WordSorter.sortByLength("Hello my friend"));
         assertEquals("a day Have wonderful", WordSorter.sortByLength("Have a wonderful day"));
+        assertNotEquals("My loves son pineapples", WordSorter.sortByLength("My son loves pineapples"));
     }
 }
