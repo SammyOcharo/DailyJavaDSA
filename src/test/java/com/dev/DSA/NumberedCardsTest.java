07424f290fd9aac40910a324f3ab55be42ebd360
sammy obanyi ocharo <obanyisammy@gmail.com>
2024-03-30 23:16:59 +0300

    numbered cards

--- src/test/java/com/dev/DSA/NumberedCardsTest.java
+++ src/test/java/com/dev/DSA/NumberedCardsTest.java
@@ -11,7 +11,9 @@ class NumberedCardsTest {
 
         int[] arr1 = {2, 5, 2, 6, 9};
         int[] arr2 = {3, 7, 3, 1, 2};
+        int [] arr3 = {};
         assertTrue(NumberedCards.winRound(arr1, arr2));
         assertFalse(NumberedCards.winRound(arr1, arr1));
+        assertFalse(NumberedCards.winRound(arr3, arr3));
     }
 }
