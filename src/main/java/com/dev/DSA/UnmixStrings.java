7211a71b8593065fb02971906c51814dd5b066ec
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-04-06 23:27:12 +0300

    unmix strings test

--- src/main/java/com/dev/DSA/UnmixStrings.java
+++ src/main/java/com/dev/DSA/UnmixStrings.java
@@ -20,12 +20,13 @@ public class UnmixStrings {
 
     public static String unmix(String str){
         char[] chars = str.toCharArray();
-        for(int i = 0; i < chars.length - 1; i +=2){
+        for(int i = 0; i < chars.length - 1; i += 2){
             char temp = chars[i];
             chars[i] = chars[i+1];
             chars[i+1] = temp;
         }
-
         return new String(chars);
     }
+
+
 }
