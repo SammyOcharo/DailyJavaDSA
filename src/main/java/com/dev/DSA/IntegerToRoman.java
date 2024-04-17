1d7d3d11089fcb92059c075f466e1669ce540fa2
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-04-17 17:12:30 +0300

    change int to roman

---
+++ src/main/java/com/dev/DSA/IntegerToRoman.java
@@ -0,0 +1,78 @@
+package com.dev.DSA;
+
+import java.util.*;
+
+public class IntegerToRoman {
+    //Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+    //
+    //Symbol       Value
+    //I             1
+    //V             5
+    //X             10
+    //L             50
+    //C             100
+    //D             500
+    //M             1000
+    //For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+    //
+    //Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+    //
+    //I can be placed before V (5) and X (10) to make 4 and 9.
+    //X can be placed before L (50) and C (100) to make 40 and 90.
+    //C can be placed before D (500) and M (1000) to make 400 and 900.
+    //Given an integer, convert it to a roman numeral.
+    //
+    //
+    //
+    //Example 1:
+    //
+    //Input: num = 3
+    //Output: "III"
+    //Explanation: 3 is represented as 3 ones.
+    //Example 2:
+    //
+    //Input: num = 58
+    //Output: "LVIII"
+    //Explanation: L = 50, V = 5, III = 3.
+    //Example 3:
+    //
+    //Input: num = 1994
+    //Output: "MCMXCIV"
+    //Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+    public static String intToRoman(int number){
+        Map<Character, Integer> symbolValueMap = new HashMap<>();
+        symbolValueMap.put('I', 1);
+        symbolValueMap.put('V', 5);
+        symbolValueMap.put('X', 10);
+        symbolValueMap.put('L', 50);
+        symbolValueMap.put('C', 100);
+        symbolValueMap.put('D', 500);
+        symbolValueMap.put('M', 1000);
+
+        System.out.println("Number: " + number);
+        System.out.println("Denominations:");
+
+        // Create a list of symbols in descending order based on their values
+        List<Character> symbols = new ArrayList<>(symbolValueMap.keySet());
+        symbols.sort(Comparator.comparingInt(symbolValueMap::get).reversed());
+        System.out.println(symbols);
+
+        StringBuilder roman = new StringBuilder();
+
+        for(char symbol: symbols){
+            System.out.println(symbol);
+            int value = symbolValueMap.get(symbol);
+            System.out.println(value);
+
+            while(number >= value){
+                roman.append(symbol);
+                number -= value;
+            }
+        }
+
+        System.out.println(roman);
+
+        return roman.toString();
+    }
+}
